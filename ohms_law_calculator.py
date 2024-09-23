5bd565eacc07dc9ec4a794f0671deb239e150d2a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-23 21:10:55 +0800

    If block is working.

--- ohms_law_calculator.py
+++ ohms_law_calculator.py
@@ -4,8 +4,25 @@
 print("What do you want to calculate?\n1 = Voltage\n2 = Current\n3 = Resistance\nPick from 1-3")
 ask_what_to_calculate = input("Input your choice here: ")
 
-# # - Formulas
-# voltage = current * resistance
+# # - Conditional Statement: If Voltage was chosen.
+def find_voltage():
+    if ask_what_to_calculate == "1":
+        print("If so, give me the values of current and resistance so that we could find the value of the voltage.")
+        ask_user_for_current_value = float(input("Current Value: "))
+        ask_user_for_resistance_value = float(input("Resistance Value: "))
+        voltage = ask_user_for_current_value * ask_user_for_resistance_value
+        print(f"The value of the voltage is {voltage}V")
+    elif ask_what_to_calculate == "2":
+        print("If so, give me the values of voltage and resistance so that we could find the value of the current.")
+        try:
+            ask_user_for_voltage_value = float(input("Voltage Value: "))
+            ask_user_for_resistance_value = float(input("Resistance Value: "))
+            current = ask_user_for_voltage_value / ask_user_for_resistance_value
+            print(f"The value of the voltage is {current}")
+        except:
+            ZeroDivisionError
+
+find_voltage()
 
 # - Ask user for the values of the remaining variables or quantities.
 # - Computation using conditional statement
