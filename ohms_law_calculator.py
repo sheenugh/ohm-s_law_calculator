61d4ada8971df4c7d25c75cd499c39d858e5045e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-23 20:46:20 +0800

    Addng PSEUDO CODE.

---
+++ ohms_law_calculator.py
@@ -0,0 +1,7 @@
+# =========== PSEUDO CODE ==========
+# || ACTUAL CODE ||
+# - Formulas
+# - Ask the user what they want to calculate: Voltage, Current, or Resistance.
+# - Ask user for the values of the remaining variables or quantities.
+# - Computation using conditional statement
+# - Printing the result
