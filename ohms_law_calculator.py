0b369af9cb1b26471ba9fd6fecfa428d8a6e204c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-09-23 21:02:07 +0800

    Prompt from user what to calculate is working.

--- ohms_law_calculator.py
+++ ohms_law_calculator.py
@@ -1,7 +1,12 @@
 # =========== PSEUDO CODE ==========
 # || ACTUAL CODE ||
-# - Formulas
 # - Ask the user what they want to calculate: Voltage, Current, or Resistance.
+print("What do you want to calculate?\n1 = Voltage\n2 = Current\n3 = Resistance\nPick from 1-3")
+ask_what_to_calculate = input("Input your choice here: ")
+
+# # - Formulas
+# voltage = current * resistance
+
 # - Ask user for the values of the remaining variables or quantities.
 # - Computation using conditional statement
 # - Printing the result
